b0ac5c304d705b8e52244b0a8dfc15786423c59b
Niloy <User@LANGERHAM>
2025-06-10 12:02:02 +0600

    First Migration

---
+++ Migrations/202506100600200_CreateUserTable.cs
@@ -0,0 +1,16 @@
+﻿namespace Law_Firm_EMS.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateUserTable : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
