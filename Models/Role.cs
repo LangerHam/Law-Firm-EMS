c6bd6d93335f60e5bbce59f1f9d83fe7f218f6c5
Niloy <User@LANGERHAM>
2025-06-11 23:41:03 +0600

    Creating Models

---
+++ Models/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Law_Firm_EMS.Models
+{
+	public class Role
+	{
+		[Key]
+		public int RoleId { get; set; }
+		[StringLength(50)]
+        public string RoleName { get; set; }
+		public virtual ICollection<Users> User { get; set; }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Law_Firm_EMS.Models
+{
+	public class Users
+	{
+        [Key]
+        public int UserID { get; set; }
+        [Required]
+        [Index(IsUnique = true)]
+        [StringLength(256)]
+        public string Email { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int RoleID { get; set; }
+        [ForeignKey("RoleID")]
+        public virtual Role Role { get; set; }
+
+        public virtual Client Client { get; set; }
+        public virtual Consultant Consultant { get; set; }
+        public virtual HR HR { get; set; }
+    }
+}
