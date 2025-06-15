ade7205f68d50fe794ac08f73e3c06b51385aa96
Niloy <User@LANGERHAM>
2025-06-15 13:38:39 +0600

    Misc

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Law_Firm_EMS.Context;
+using Law_Firm_EMS.Models;
 
 namespace Law_Firm_EMS.Controllers
 {
@@ -12,6 +14,41 @@ namespace Law_Firm_EMS.Controllers
         public ActionResult LandingPage()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult RequestConsultation(string name, string email, string caseDetails)
+        {
+            try
+            {
+                using (var db = new LawContextDb())
+                {
+                    var newUser = new Users
+                    {
+                        Email = email,
+                        PasswordHash = "temporary_password",
+                        RoleID = 2, 
+                        CreatedAt = System.DateTime.UtcNow
+                    };
+
+                    var newClient = new Client
+                    {
+                        FullName = name,
+                        CaseDetails = caseDetails,
+                        StatusID = 1, 
+                        User = newUser
+                    };
+                    db.ClientEntity.Add(newClient);
+
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("LandingPage");
+            }
+            catch (System.Exception ex)
+            {
+                return RedirectToAction("LandingPagee");
+            }
         }
     }
 }
