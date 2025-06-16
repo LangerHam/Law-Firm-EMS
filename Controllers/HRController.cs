723063620e68d45cd6c33284e33eef3040bc867e
Niloy <User@LANGERHAM>
2025-06-16 18:25:09 +0600

    HR Dashboard update

--- Controllers/HRController.cs
+++ Controllers/HRController.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Law_Firm_EMS.Context;
+using Law_Firm_EMS.ViewModels;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +11,41 @@ namespace Law_Firm_EMS.Controllers
 {
     public class HRController : Controller
     {
-        // GET: HR
+        private LawContextDb db;
+        public HRController()
+        {
+            db = new LawContextDb();
+        }
+
         public ActionResult Dashboard()
         {
-            return View();
+            if (Session["RoleID"] == null || (int)Session["RoleID"] != 1)
+            {
+                return RedirectToAction("Login", "Login");
+            }
+
+            var pendingStatus = db.StatusTypeEntity.FirstOrDefault(s => s.StatusName.ToLower() == "pending");
+
+            var viewModel = new HRDashboardViewModel();
+
+            if (pendingStatus != null)
+            {
+                int pendingStatusId = pendingStatus.StatusID;
+
+                viewModel.NewClientRequests = db.ClientEntity
+                    .Where(c => c.StatusID == pendingStatusId)
+                    .Include(c => c.User)
+                    .OrderByDescending(c => c.User.CreatedAt)
+                    .ToList();
+
+                viewModel.PendingLeaveRequests = db.LeaveEntity
+                    .Where(l => l.StatusID == pendingStatusId)
+                    .Include(l => l.RequestedByConsultant.User)
+                    .OrderBy(l => l.FromDate)
+                    .ToList();
+            }
+
+            return View(viewModel);
         }
     }
 }
